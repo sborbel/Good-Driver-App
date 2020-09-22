b8d013e676d2d41db7081f21e02ac1b3fd711bbe
Sean Borbely <sborbel@clemson.edu>
2020-09-21 21:35:27 -0400

    Revert Back To Last Good Commit

--- services/client/src/components/About.jsx
+++ services/client/src/components/About.jsx
@@ -5,8 +5,7 @@ const About = () => (
     <h1 className="title is-1">About</h1>
     <hr />
     <br />
-    <p className="content">Add something relevant here.</p>
+    <p className="content">Add something relevant here. </p>
   </div>
 );
-
 export default About;
