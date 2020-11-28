339e6c5dc46e902410b0e33eed873ea13cd421d0
Wes Bailey <espressoandcode@gmail.com>
2020-11-28 11:52:40 -0500

    Add 'fee report' route to API

--- services/server/project/api/reports/views.py
+++ services/server/project/api/reports/views.py
@@ -1,9 +1,9 @@
+# services/server/project/api/reports/views.py
 
 from flask import current_app
 from flask import request
 from flask_restx import Resource, fields, Namespace
 from flask import make_response
-
 
 from project.api.users.crud import (
     get_all_users,
@@ -39,7 +39,31 @@ class ReportsAffiliations(Resource):
         response.mimetype='text/csv'
         return response
 
+class ReportsFees(Resource):
+    def get(self):
+        order_fields = ["id","status","user_id","sponsor_name","created_date"]
+        order_item_fields = ["id","order_id","catalog_id","catalog_item_id","quantity","actual_cost","points_cost","created_date"]
+        fee_report_fields = ['order_id', 'order_created_date', 'status', 'user_id', 'sponsor_name',  'catalog_id', 'catalog_item_id', 'item_id', 'quantity', 'actual_cost', 'points_cost', 'item_added_date']
+        fee_report_data = [] 
+        order_data = get_orders()
+        for order in order_data:
+            order['order_id'] = order.pop('id')
+            order['order_created_date'] = order.pop('created_date')
+            order_item_data = get_items_by_order_id(order['order_id'])
+            for item in order_item_data:
+                item['item_id'] = item.pop('id')
+                item['item_added_date'] = item.pop('created_date')
+                item.pop('order_id')
+                temp_data = order.copy()
+                temp_data.update(item)
+                fee_report_data.append(temp_data)
 
+        report_data = build_csv(fee_report_data, fee_report_fields)
+        response = make_response(report_data)
+        cd = 'attachment; filename=fee_report.csv'
+        response.headers['Content-Disposition'] = cd 
+        response.mimetype='text/csv'
+        return response
 
 ####Helpers
 
@@ -91,14 +115,44 @@ def get_affiliations():
 
 ###
 
+orders_namespace = Namespace("orders")
 
+order = orders_namespace.model(
+    "Order",
+    {
+        "id": fields.Integer(readOnly=True),
+        "status": fields.String(required=False),
+        "user_id": fields.Integer(required=True),
+        "sponsor_name": fields.String(required=True),
+        "created_date": fields.DateTime,
+    },
+)
 
+@orders_namespace.marshal_with(order, as_list=True)
+def get_orders():
+    return get_all_orders()
 
+order_items_namespace = Namespace("order_items")
 
+order_item = order_items_namespace.model(
+    "OrderItem",
+    {
+        "id": fields.Integer(readOnly=True),
+        "order_id": fields.Integer(required=True),
+        "catalog_id": fields.Integer(required=True),
+        "catalog_item_id": fields.Integer(required=True),
+        "quantity": fields.Integer(required=True),
+        "actual_cost": fields.Fixed(decimals=2, required=True),
+        "points_cost": fields.Integer(required=True),
+        "created_date": fields.DateTime,
+    },
+)
 
+@order_items_namespace.marshal_with(order_item, as_list=True)
+def get_items_by_order_id(order_id):
+    return get_all_order_items_by_order_id(order_id)
 
-
-
+###
 
 def build_csv(file, attrs):
     res = ""
@@ -114,6 +168,6 @@ def build_csv(file, attrs):
 
 reports_namespace.add_resource(ReportsUsers, "/users")
 reports_namespace.add_resource(ReportsAffiliations, "/affiliations")
-
+reports_namespace.add_resource(ReportsFees, "/fees")
 
 
